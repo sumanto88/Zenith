f56ec433f3bf013d762145bd171cb60cd12d90f2
sumanto88 <rizkia.slavic@gmail.com>
2025-05-21 09:34:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("KantorBola88")
 st.write(
-    "Kang Dedi Barrack Obamaa"
+    "Kang Dedi Barrack Obama"
 )
 st.image("IMG_20250519_090627.jpg", width=200) 
 st.image("IMG_20250219_101505.jpg", width=200) 
