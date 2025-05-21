e5fc0f17db1e81c7d6ed57abfea775b34a9547c7
sumanto88 <rizkia.slavic@gmail.com>
2025-05-21 09:33:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("KantorBola88")
 st.write(
     "Kang Dedi Barrack Obamaa"
 )
-st.image("IMG_20250519_090627.jpg", widht=200) 
-st.image("IMG_20250219_101505.jpg", widht=200) 
+st.image("IMG_20250519_090627.jpg", width=200) 
+st.image("IMG_20250219_101505.jpg", width=200) 
